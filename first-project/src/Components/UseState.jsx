2dab479fc3d8dc59df3078f8a6f51845b990b744
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-01-14 18:15:52 +0530

    In that add Advanced leve usestate hook

--- first-project/src/Components/UseState.jsx
+++ first-project/src/Components/UseState.jsx
@@ -29,10 +29,24 @@ function UseState(){
         )
     }
 
+    function Advance(){
+
+        const [val,setVal] = useState({name:"score",value:false});
+
+        return(
+            <div className='p-4 '>
+                <h1>{val.name}</h1>
+                <h1>{val.value.toString()}</h1>
+                <button onClick={()=>setVal({...val,name:"true"})} className='px-2 py-2 bg-blue-400 rounded-full text-white mt-4'>Change</button>
+            </div> 
+        )
+    }
+
     return(
         <>
             <Basic />
             <Intermediate />
+            <Advance />
         </>
     )
 }
